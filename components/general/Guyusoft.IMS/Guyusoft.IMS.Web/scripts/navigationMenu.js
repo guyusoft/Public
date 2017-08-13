5b53f25d33c17f8e5d193d78a73f61fca0209c05
AlexSuDev <alexsudev@163.com>
2017-08-13 21:16:00 +0800

    template

--- components/general/Guyusoft.IMS/Guyusoft.IMS.Web/scripts/navigationMenu.js
+++ components/general/Guyusoft.IMS/Guyusoft.IMS.Web/scripts/navigationMenu.js
@@ -15,67 +15,19 @@ app.controller('navigationMenuController', ['$scope', 'ngDialog', function ($sco
         });
     }
 
-    $scope.edit = function (id) {
+    $scope.edit = function (item) {
         ngDialog.open({
-            template:
-              '<div class="panel panel-default dialog"> \
-                 <div class="panel-heading">新增链接</div> \
-                 <div class="panel-body"> \
-                    <form class="form-horizontal ng-pristine ng-valid"> \
-                       <div class="form-group"> \
-                          <label class="col-lg-4 control-label">链接标题</label> \
-                          <div class="col-lg-8"> \
-                             <input type="text" placeholder="链接标题" class="form-control"> \
-                          </div> \
-                       </div> \
-                       <div class="form-group"> \
-                          <label class="col-lg-4 control-label">链接地址</label> \
-                          <div class="col-lg-8"> \
-                             <input type="text" placeholder="链接地址" class="form-control"> \
-                          </div> \
-                       </div> \
-                       <div class="form-group"> \
-                          <div class="col-lg-offset-4 col-lg-8"> \
-                             <button type="button" class="btn btn-sm btn-default">确定</button> \
-                             <button type="button" class="btn btn-sm btn-default">取消</button> \
-                          </div> \
-                       </div> \
-                    </form> \
-                 </div> \
-            </div>',
-            plain: true
+            template: 'navigationMenuAddAndEdit',
+            className: 'ngdialog-theme-default',
+            data: item
         });
     };
 
-    $scope.add = function (id) {
+    $scope.add = function (item) {
         ngDialog.open({
-            template:
-              '<div class="panel panel-default dialog"> \
-                 <div class="panel-heading">新增链接</div> \
-                 <div class="panel-body"> \
-                    <form class="form-horizontal ng-pristine ng-valid"> \
-                       <div class="form-group"> \
-                          <label class="col-lg-4 control-label">链接标题</label> \
-                          <div class="col-lg-8"> \
-                             <input type="text" placeholder="链接标题" class="form-control"> \
-                          </div> \
-                       </div> \
-                       <div class="form-group"> \
-                          <label class="col-lg-4 control-label">链接地址</label> \
-                          <div class="col-lg-8"> \
-                             <input type="text" placeholder="链接地址" class="form-control"> \
-                          </div> \
-                       </div> \
-                       <div class="form-group"> \
-                          <div class="col-lg-offset-4 col-lg-8"> \
-                             <button type="button" class="btn btn-sm btn-default">确定</button> \
-                             <button type="button" class="btn btn-sm btn-default">取消</button> \
-                          </div> \
-                       </div> \
-                    </form> \
-                 </div> \
-            </div>',
-            plain: true
+            template:'navigationMenuAddAndEdit',
+            className: 'ngdialog-theme-default',
+            data: item
         });
     };
 
